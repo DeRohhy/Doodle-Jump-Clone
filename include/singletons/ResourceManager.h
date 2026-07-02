31ea360d89253e49a4160af81e10e871b81c8c2c
RohhyDohhy <rohhy.business@gmail.com>
2026-07-03 01:56:42 +0330

    fix(ResourceManager): convert ResourceManager.cpp content to ResourceManager.h becuase of template

--- include/singletons/ResourceManager.h
+++ include/singletons/ResourceManager.h
@@ -3,6 +3,7 @@
 #include "singletons/Singleton.h"
 
 #include <unordered_map>
+#include <stdexcept>
 #include <string>
 
 template<typename type>
@@ -12,3 +13,15 @@ public:
 private:
     std::unordered_map<std::string, type> resources;
 };
+
+template<typename type>
+type& ResourceManager<type>::get(const std::string& file_path) {
+    if (resources.find(file_path) == resources.end()) {
+        type new_resource;
+        if (!new_resource.loadFromFile(file_path)) {
+            throw std::runtime_error("ResourceManager: Couldn't load resource: " + file_path);
+        }
+        resources[file_path] = std::move(new_resource);
+    }
+    return resources[file_path];
+}

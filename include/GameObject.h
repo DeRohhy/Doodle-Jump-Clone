904cf60ae83cd2af733ece43afaeae65ca4ee9e1
RohhyDohhy <rohhy.business@gmail.com>
2026-07-02 16:27:16 +0330

    feat(GameObject): add GameObject class add position and velocity member variables add virtual Start() and Update(delta) functions

---
+++ include/GameObject.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+class GameObject {
+public:
+    GameObject(sf::Vector2f _position, sf::Vector2f _velocity)
+        : position(_position), velocity(_velocity) {}
+    virtual ~GameObject() = default;
+
+    virtual void start() {}
+    virtual void update(float delta) {}
+
+    sf::Vector2f getPosition() const { return position; }
+    sf::Vector2f getVelocity() const { return velocity; }
+
+    void setPosition(const sf::Vector2f& new_position) { position = new_position; };
+    void setVelocity(const sf::Vector2f& new_velocity) { velocity = new_velocity; };
+protected:
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+};

28d7e0de355f215835f9b11b06c39b2b1dd44f1c
RohhyDohhy <rohhy.business@gmail.com>
2026-07-05 22:44:41 +0330

    refactor(Player): refactor player looks scale player_sprite to fit screen proportions better add offset to player_sprite so that the bottom of the feet is at local y = 0 and the center between player legs is at local x = 0 feat: make player_sprite point to facing direction

--- include/Player.h
+++ include/Player.h
@@ -6,10 +6,16 @@
 #include <string>
 #include <optional>
 
+enum Direction {
+    RIGHT,
+    LEFT
+};
+
 class Player : public GameObject {
 public:
     Player(sf::Vector2f _position, sf::Vector2f _velocity)
         : GameObject(_position, _velocity) {}
+
     void start() override;
     void update(float delta) override;
     void render(sf::RenderWindow& window) override;
@@ -17,19 +23,19 @@ public:
 
 private:
     const std::string RIGHT_DOODLE_PATH = "assets/right_doodle.png";
-    const std::string LEFT_DOODLE_PATH = "assets/left_doodle.png";
-
+    static constexpr float SPRITE_SCALE = 0.7f;
     static constexpr float JUMP_FACTOR = 800.f;
     static constexpr float TOP_SPEED = 700.f;
     static constexpr float VELOCITY_POWER = 0.99f;
     static constexpr float ACCEL_RATE = 7.f;
 
-    sf::Texture left_doodle_texture;
     sf::Texture right_doodle_texture;
     // SFML 3 removed sf::Sprite’s default constructor
     // so we'll get an error if we say:
     // sf::Sprite player_sprite;
     std::optional<sf::Sprite> player_sprite; 
 
-    bool is_facing_right;
+    Direction facing_direction = Direction::RIGHT;
+
+    void setDirection(Direction new_direction);
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -2,21 +2,26 @@
 
 #include "singletons/ResourceManager.h"
 #include "singletons/GameConfig.h"
-#include <iostream>
 #include <math.h>
 
 void Player::start() {
-    left_doodle_texture = ResourceManager<sf::Texture>::getInstance().get(LEFT_DOODLE_PATH);
     right_doodle_texture = ResourceManager<sf::Texture>::getInstance().get(RIGHT_DOODLE_PATH);
 
-    player_sprite.emplace(left_doodle_texture); // construct in-place with texture
+    player_sprite.emplace(right_doodle_texture); // construct in-place with texture
+
+    // offset player sprite so that
+    // the bottom of the feet is at local y = 0
+    // and the center between player legs is at local x = 0
+    sf::Vector2f local_bound = player_sprite->getLocalBounds().size;
+    player_sprite->setOrigin({3.0f * local_bound.x / 8.0f, local_bound.y});
+
+    player_sprite->setScale({SPRITE_SCALE, SPRITE_SCALE});
 }
 
 void Player::update(float delta) {
     handleMovement(delta);
     velocity.y += GameConfig::GRAVITY * delta;
     position += velocity * delta;
-
 
     if (player_sprite) player_sprite->setPosition(position);
 }
@@ -30,17 +35,27 @@ void Player::handleMovement(float delta) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) direction = -1;
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) direction = 1;
 
-    if (direction == 1 && !is_facing_right) {
-        is_facing_right = true;
-        player_sprite->setTexture(right_doodle_texture);
-    } else if (direction == -1 && is_facing_right) {
-        is_facing_right = false;
-        player_sprite->setTexture(left_doodle_texture);
-    }
+    if (direction != 0)
+        setDirection(direction == -1 ? Direction::LEFT : Direction::RIGHT);
 
     float target_speed = static_cast<float>(direction) * TOP_SPEED;
     float speed_difference = target_speed - velocity.x;
     float delta_velocity = std::pow(std::abs(speed_difference) * ACCEL_RATE, VELOCITY_POWER) * (speed_difference > 0 ? 1 : -1);
 
     velocity.x += delta_velocity * delta;
+}
+
+
+void Player::setDirection(Direction new_direction) {
+    if (facing_direction == new_direction) return; 
+        
+    facing_direction = new_direction;
+
+    if (!player_sprite) return;
+
+    if (new_direction == Direction::LEFT) {
+        player_sprite->setScale({-SPRITE_SCALE, SPRITE_SCALE});
+    } else {
+        player_sprite->setScale({SPRITE_SCALE, SPRITE_SCALE});
+    }
 }

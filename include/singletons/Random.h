3ad7f51bb586c173e0445057d5f69ef1b36c7f87
RohhyDohhy <rohhy.business@gmail.com>
2026-07-08 16:35:10 +0330

    bug(Random): put randomFloatRange in header

--- include/singletons/Random.h
+++ include/singletons/Random.h
@@ -5,12 +5,10 @@
 
 class Random : public Singleton<Random> {
 public:
-    float randomFloatRange(float low, float high);
-};
-
-float Random::randomFloatRange(float low, float high) {
+    float randomFloatRange(float low, float high) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     std::uniform_real_distribution<float> dis(low, high);
     return dis(gen);
-}
+    }
+};

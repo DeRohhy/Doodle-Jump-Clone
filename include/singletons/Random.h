773198eee459d922654f87a5cc70df0cdaa23486
RohhyDohhy <rohhy.business@gmail.com>
2026-07-08 15:24:02 +0330

    feat(Random): aadd Random.h singleton class implement randomFloatRange()

---
+++ include/singletons/Random.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "singletons/Singleton.h"
+#include <random>
+
+class Random : public Singleton<Random> {
+public:
+    float randomFloatRange(float low, float high);
+};
+
+float Random::randomFloatRange(float low, float high) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(low, high);
+    return dis(gen);
+}

7d7db34a8146000373c9c61f0d9bf56abe20b6db
RohhyDohhy <rohhy.business@gmail.com>
2026-07-27 01:55:13 +0330

    feat(Hole): add Hole class

---
+++ include/Hole.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "GameObject.h"
+
+class Hole : public GameObject {
+public:
+    Hole(sf::Vector2f _position, bool small_hole);
+
+    void render(sf::RenderWindow& window) override;
+
+    sf::FloatRect getBounds();
+private:
+    static inline const std::string SPRITE_PATH = "assets/hole.png";
+    sf::Texture hole_texture;
+    std::optional<sf::Sprite> hole_sprite; 
+};
---
+++ src/Hole.cpp
@@ -0,0 +1,34 @@
+#include "Hole.h"
+
+#include "singletons/ResourceManager.h"
+#include "Theme.h"
+
+Hole::Hole(sf::Vector2f _position, bool small_hole) : GameObject(_position, sf::Vector2f()) {
+    hole_texture = ResourceManager<sf::Texture>::getInstance().get(SPRITE_PATH);
+
+    hole_sprite.emplace(hole_texture);
+    
+    sf::Vector2f local_bound = hole_sprite->getLocalBounds().size;
+    hole_sprite->setOrigin({local_bound.x / 2.f, local_bound.y / 2.f});
+
+    if (small_hole) {
+        hole_sprite->setScale({Theme::SCALE_SMALL, Theme::SCALE_SMALL});
+    } else {
+        hole_sprite->setScale({Theme::SCALE_MEDIUM, Theme::SCALE_MEDIUM});
+    }
+    hole_sprite->setPosition(position);
+}
+
+sf::FloatRect Hole::getBounds() {
+    if (!hole_sprite) {
+        return sf::FloatRect();
+    }
+    
+    return hole_sprite->getGlobalBounds();
+}
+
+void Hole::render(sf::RenderWindow& window) {
+    if (hole_sprite) {
+        window.draw(hole_sprite.value());
+    }
+}

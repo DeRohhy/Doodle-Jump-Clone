bec02b70728f25007bb97bda593600ef534c40c1
RohhyDohhy <rohhy.business@gmail.com>
2026-07-27 12:09:44 +0330

    feat(ResourceManager): add template specialization for loading music

--- include/singletons/ResourceManager.h
+++ include/singletons/ResourceManager.h
@@ -6,6 +6,9 @@
 #include <stdexcept>
 #include <string>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
 template<typename type>
 class ResourceManager : public Singleton<ResourceManager<type>> {
 public:
@@ -34,6 +37,19 @@ inline sf::Font& ResourceManager<sf::Font>::get(const std::string& file_path) {
             throw std::runtime_error("ResourceManager: Couldn't load font: " + file_path);
         }
         resources[file_path] = std::move(new_resource);
+    }
+    return resources[file_path];
+}
+
+template<>
+inline sf::Music& ResourceManager<sf::Music>::get(const std::string& file_path) {
+    if (resources.find(file_path) == resources.end()) {
+        sf::Music& new_resource = resources[file_path]; 
+        
+        if (!new_resource.openFromFile(file_path)) {
+            resources.erase(file_path); // Clean up if it fails
+            throw std::runtime_error("ResourceManager: Couldn't load music: " + file_path);
+        }
     }
     return resources[file_path];
 }
